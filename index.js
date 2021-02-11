7616e0725f355c776fe95d29f59b920b579acf6e
Marcus Del Vecchio <marcusdel2112@rogers.com>
2021-02-10 21:49:37 -0500

    Setup 'create qualifitcations' functionality for instructors
    - front end html elements for inputting qualifications information
    - backend routes for posting updated course info with newly defined qualifications properties
    - data service for submitting the course qualifications to the db
    - created dummy Material UI table to be used as a list of courses for instructors

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const express = require("express");
 const Joi = require("joi");
 const MongoClient = require('mongodb').MongoClient;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+//const bcrypt = require('bcrypt');
+//const jwt = require('jsonwebtoken');
 
 const app = express();
 const router = express.Router();
@@ -16,33 +16,77 @@ app.use(express.json());
 app.use('/', express.static('static'));
 app.use(cors());
 
-const uri = "mongodb+srv://node:"+process.env.DB_PASSWORD_SECRET+"@uwo-se.0zbtu.mongodb.net/SE3350-TA-Course-Matching?retryWrites=true&w=majority";
+const uri = "mongodb+srv://node:" + process.env.DB_PASSWORD_SECRET + "@uwo-se.0zbtu.mongodb.net/SE3350-TA-Course-Matching?retryWrites=true&w=majority";
 const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-/* sample code
-
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-*/
-
 // ######## logs ########
-	app.use((req, res, next) => {
-	    console.log(`${req.method} request for ${req.url}`);
-	    next();
-	});
+app.use((req, res, next) => {
+	console.log(`${req.method} request for ${req.url}`);
+	next();
+});
 
 // ######## routes ########
 
-	router.route('/test')
+router.route('/test')
 	// get all courses
-		.get((req, res) => {
-			res.status(200).send("Hello world");
-		})
+	.get((req, res) => {
+		res.status(200).send("Hello world");
+	})
 
 app.use('/api', router);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// getting all courses 
+
+// req.body format = { course: "some-course", qualifications: "some text here" }
+router.route('/courses-insert-qualifications')
+	.post((req, res) => {
+
+		// sanitize body with schema
+		const schema = Joi.object({
+			course: Joi.string().trim().required(),
+			qualifications: Joi.string().trim().required()
+		});
+		const result = schema.validate(req.body);
+
+		if (result.error) return res.status(400).send(result.error); //.error.details[0].message)
+
+		return mongoClient.connect().then(() => {
+
+			let collection = mongoClient.db("SE3350-TA-Course-Matching").collection("courses").find();
+
+			// return promise that checks if that course exists
+			return new Promise((resolve, reject) => {
+
+				collection.forEach(e => {
+					if (e.course.toLowerCase() === req.body.course.toLowerCase()) {
+						resolve(e);
+					}
+				},
+					() => {
+						collection.close();
+						reject();
+					});
+			})
+				.then((result) => {
+					// if course exists
+
+					let newCourse = result;
+
+					newCourse.qualifications = req.body.qualifications;
+
+					return mongoClient.db("SE3350-TA-Course-Matching").collection("courses").deleteOne({ _id: result._id }).then(() => {
+						return mongoClient.db("SE3350-TA-Course-Matching").collection("courses").insertOne(newCourse).then(() => {
+							return res.status(200).send(req.body);
+						});
+					});
+				})
+				.catch(() => {
+					// if course NOT exist
+					mongoClient.db("SE3350-TA-Course-Matching").collection("courses").insertOne(req.body);
+
+					return res.status(200).send(req.body);
+				});
+		})
+	});
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { InstructorComponentComponent } from './instructor-component/instructor-component.component';
+
+// angular material table
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstructorComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
