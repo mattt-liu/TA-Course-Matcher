6f74e2fa6918876cc17ed3fb1110ceed6f309fc0
sidugboe <71351551+sidugboe@users.noreply.github.com>
2021-03-09 23:51:22 +0100

    display questions (#5)
    display course info and questions

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-
+import { CourseInfoAndQuestionsComponent } from './course-info-and-questions/course-info-and-questions.component'
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionsMlComponent } from './questions-ml/questions-ml.component';
 import { RequireTAPositionsComponent } from './require-tapositions/require-tapositions.component';
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'questions', component: QuestionsMlComponent },
   { path: 'RequireTAPositionsComponent', component: RequireTAPositionsComponent},
   { path: 'hours', component: HoursComponent},
+  { path: 'Info', component: CourseInfoAndQuestionsComponent},
 ]
 
 @NgModule({
