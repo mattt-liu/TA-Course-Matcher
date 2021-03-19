fe538449076a9353418735575824246969e752a6
emiranda2 <emirand2@uwo.ca>
2021-03-18 21:30:08 -0400

    added file upload

--- my-app/src/app/upload-rankings/upload-rankings.component.ts
+++ my-app/src/app/upload-rankings/upload-rankings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import { UploadApplicantService } from '../upload-applicant.service';
 
 @Component({
   selector: 'app-upload-rankings',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadRankingsComponent implements OnInit {
 
-  constructor() { }
+    data: any[];
+    csvError: boolean = false;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private csvParser: NgxCsvParser,
+        private uploadService: UploadApplicantService
+        ) { }
 
+    @ViewChild('fileImportInput', { static: false }) fileImportInput: any;
+
+    ngOnInit(): void {
+    }
+
+    fileListener($event: any): void {
+
+        this.csvError = false;
+
+        const files = $event.srcElement.files;
+        this.csvParser.parse(files[0], { header: false, delimiter: ',' }).pipe()
+            .subscribe((data: Array<any>) => {
+                this.data = data;
+            }, (err: NgxCSVParserError) => {
+                console.log(err);
+                this.csvError = true;
+            })
+    }
 }
