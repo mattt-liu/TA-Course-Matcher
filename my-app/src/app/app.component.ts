3a89967b63a5d05dc804afcf97794845fc1f1c01
Matt <mattliuusa@hotmail.com>
2021-03-30 21:04:42 -0400

    add login checking

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+
+	user = undefined;
+
+	constructor(
+		private userService: UserService,
+	) { }
+
+	ngOnInit(): void {
+		this.checkLogin();
+	}
+
+	checkLogin() {
+		this.userService.getEmail().subscribe(data => {
+			this.user = data;
+		},
+			err => {
+				this.user = undefined;
+			});
+	}
+
+	logout() {
+		console.log("LOGOUT");
+		this.userService.logout();
+		this.user = undefined;
+	}
 }
