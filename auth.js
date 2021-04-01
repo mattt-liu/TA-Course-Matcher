302062a812ae50505a2ea8c9a7167d910fafb874
Matt <mattliuusa@hotmail.com>
2021-04-01 18:19:15 -0400

    update token signing

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const secret = "c5fbee81f66de269876ade1db24d7e9a"; /* testing purposes only */
-const adminSecret = "249c672b888f18e333763baceb40fa36"; /* testing purposes only */
 
 function authenticateToken(req, res, next) {
     /* when a token is sent in the header
@@ -13,38 +12,14 @@ function authenticateToken(req, res, next) {
 
     // if there is no token then redirect
     if (!token) {
-        return res.status(400).send("Bad token"); // TODO: redirect
+        return res.status(401).json({message:"No token"}); // TODO: redirect
     }
 
     // verify token
     jwt.verify(token, secret, (err, payload) => {
 
         // if token is invalid
-        if (err) return res.status(400).send("Bad token"); // TODO: redirect 
-        req.user = payload;
-        next();
-    })
-}
-
-function authenticateAdminToken(req, res, next) {
-    /* when a token is sent in the header for admins
-     */
-
-    // parse token in header
-    const header = req.headers['authorization'];
-    const token = header && header.split(' ')[1]; // Bearer TOKEN
-
-    // if there is no token then redirect to login
-    if (!token) {
-        return res.status(400).send("Bad token"); // TODO: redirect 
-    }
-
-    // verify token
-    jwt.verify(token, adminSecret, (err, payload) => {
-
-        // if token is invalid
-        if (err) return res.status(400).send("Bad token"); // TODO: redirect login
-
+        if (err) return res.status(403).json({message:"Bad token"}); // TODO: redirect 
         req.user = payload;
         next();
     })
@@ -55,13 +30,13 @@ function signToken(user) {
 
     let userSign = {
         // TODO: figure out what to sign on the token 
-        email: user.email
+        email: user.email,
+        admin: user.admin
     }
     return jwt.sign(userSign, secret, { expiresIn: "15m" }); // TODO: create refresh token
 }
 
 module.exports = {
     authenticateToken: authenticateToken,
-    authenticateAdminToken: authenticateAdminToken,
     signToken: signToken
 }
