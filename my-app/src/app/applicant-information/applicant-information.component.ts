fd1574ff01de03e842d09427b7f23e875f71661a
Matt <mattliuusa@hotmail.com>
2021-04-02 17:47:24 -0400

    update styling

--- my-app/src/app/applicant-information/applicant-information.component.ts
+++ my-app/src/app/applicant-information/applicant-information.component.ts
@@ -12,50 +12,74 @@ export class ApplicantInformationComponent implements OnInit {
   applicants = [];
   filteredapplicants = [];
   questions = [];
+  expand: boolean[] = undefined;
+  answers = [];
 
-
-  constructor(private _configservice:ConfigService) { }
+  constructor(private _configservice: ConfigService) { }
 
   ngOnInit(): void {
-    this._configservice.getapplicants().subscribe((data) => this.applicants = data);
-    this._configservice.getquestions().subscribe((data) => this.questions = data);
+    this.fillExpand();
+    this._configservice.getapplicants().subscribe((data) => {
+      this.applicants = data;
+    });
+    this._configservice.getquestions().subscribe((data) => {
+      this.questions = data;
+      this.getAnswers();
+      console.log(this.answers);
+    });
   }
 
-  display(){
+  getAnswers() {
+    this.answers = [];
 
-    for (var key in this.applicants) {
-      this.filteredapplicants.push(this.applicants[key])
-    }
-
-    for (var i in this.filteredapplicants) { 
-      for(var j in this.filteredapplicants[i].appliedCourses) { 
-
-       for (var k in this.questions) {
-
-        if (this.filteredapplicants[i].appliedCourses[j].toUpperCase() == this.questions[k].course) {
-
-        console.log(this.filteredapplicants[i].appliedCourses[j]+"-"+this.questions[k].course) 
-        
-        //simply print the questions for that course
-        for (var l in this.questions[k].questions) {
-         console.log(this.questions[k].questions[l])
-         this.filteredapplicants[i].appliedCourses[j] = (this.filteredapplicants[i].appliedCourses[j]+"-"+this.questions[k].questions[l])
-
+    for (let applicant of this.applicants) {
+        let app = {
+          questions: [],
+          answers: applicant.answers
         }
+        for (let i = 0; i < applicant.appliedCourses.length; i ++) {
+          for (let q of this.questions) {
+            if (q.course === applicant.appliedCourses[i].toUpperCase()) {
+              app.questions.push(q.questions);
+            }
+          }
+        }
+      this.answers.push(app);
+    }
+  }
 
+  display() {
+    this.filteredapplicants = this.applicants.slice();
 
-       }
+    for (var i in this.filteredapplicants) {
+      for (var j in this.filteredapplicants[i].appliedCourses) {
+
+        for (var k in this.questions) {
+
+          if (this.filteredapplicants[i].appliedCourses[j].toUpperCase() == this.questions[k].course) {
+
+            // console.log(this.filteredapplicants[i].appliedCourses[j]+"-"+this.questions[k].course) 
+
+            //simply print the questions for that course
+            for (var l in this.questions[k].questions) {
+              //  console.log(this.questions[k].questions[l])
+              this.filteredapplicants[i].appliedCourses[j] = (this.filteredapplicants[i].appliedCourses[j] + "-" + this.questions[k].questions[l])
+
+            }
+          }
+        }
       }
+    }
+  }
 
-   }
+  bruh() {console.log("a", this.applicants, "fa", this.filteredapplicants, "q", this.questions)}
+
+  fillExpand() {
+      this.expand = [];
+      for (let i = 0; i < this.applicants.length; i++) {
+          this.expand.push(false);
+      }
+  }
 }
 
-  
 
-  
-
-}
-}
-
-
- 
